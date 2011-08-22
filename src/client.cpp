08a410b9fd59716646951c5a0cab6a78f41f1876
Dmitry Shatrov <shatrov@gmail.com>
2011-08-22 18:24:30 +0400

    Initial commit.

---
+++ src/client.cpp
@@ -0,0 +1,346 @@
+#include <libmary/libmary.h>
+
+#include <mycpp/mycpp.h>
+#include <mycpp/cmdline.h>
+
+#include <cstdlib>
+
+
+using namespace M;
+
+namespace {
+
+class Options
+{
+public:
+    bool help;
+
+    bool got_server_addr;
+    IpAddress server_addr;
+
+    Uint32 num_clients;
+    Uint32 report_interval;
+
+    Options ()
+	: help (false),
+
+	  got_server_addr (false),
+
+	  num_clients (1),
+	  report_interval (0)
+    {
+    }
+};
+
+mt_const Options options;
+
+void printUsage ()
+{
+    outs->print ("Usage: bmeter_clnt [options]\n"
+		 "Options:\n"
+		 "  -s --server-addr <address>    Server address (default: localhost:7777)\n"
+		 "  -n --num-clients <number>     Number of clients to simulate (default: 1)\n"
+		 "  -r --report-interval <number> Interval between video frame reports.\n"
+		 "                                0 means no reports. (default: 0)\n"
+		 "  -h --help                     Show help message.\n");
+    outs->flush ();
+}
+
+class ClientList_name;
+
+class Client : public Object,
+	       public IntrusiveListElement<ClientList_name>
+{
+public:
+    bool valid;
+
+    mt_const Byte id_char;
+
+    TcpConnection tcp_conn;
+
+    mt_const PollGroup::PollableKey pollable_key;
+
+    Client (Byte const id_char)
+	: valid (true),
+	  id_char (id_char),
+	  tcp_conn (this)
+    {
+    }
+};
+
+typedef IntrusiveList<Client, ClientList_name> ClientList;
+
+mt_mutex (mutex) ClientList client_list;
+
+Mutex mutex;
+
+ServerApp server_app (NULL);
+
+mt_const Byte *dummy_buf = NULL;
+Size const dummy_buf_len = 1 << 20; // 1 Mb
+
+mt_mutex (mutex) void destroyClient (Client * const client)
+{
+    if (!client->valid)
+	return;
+    client->valid = false;
+
+    server_app.getPollGroup()->removePollable (client->pollable_key);
+
+    client_list.remove (client);
+    client->unref ();
+}
+
+void processInput (void * const _client)
+{
+    Client * const client = static_cast <Client*> (_client);
+
+    // Discarding all data
+    for (;;) {
+	Size nread = 0;
+	AsyncIoResult const res = client->tcp_conn.read (Memory (dummy_buf, dummy_buf_len), &nread);
+	if (res == AsyncIoResult::Error) {
+	    logE_ (_func, "read() failed: ", exc->toString());
+	    goto _destroy_client;
+	}
+
+	if (res == AsyncIoResult::Eof ||
+	    res == AsyncIoResult::Normal_Eof)
+	{
+	    logW_ (_func, "EOF");
+	    goto _destroy_client;
+	}
+
+	if (res == AsyncIoResult::Again ||
+	    res == AsyncIoResult::Normal_Again)
+	{
+	    break;
+	}
+
+	assert (res == AsyncIoResult::Normal);
+    }
+
+    return;
+
+_destroy_client:
+    mutex.lock ();
+    destroyClient (client);
+    mutex.unlock ();
+}
+
+void processError (Exception * const exc_,
+		   void      * const _client)
+{
+    Client * const client = static_cast <Client*> (_client);
+
+    if (exc_)
+	logD_ (_func, "exc: ", exc_->toString());
+    else
+	logD_ (_func_);
+
+    mutex.lock ();
+    destroyClient (client);
+    mutex.unlock ();
+}
+
+Connection::InputFrontend const conn_input_frontend = {
+    processInput,
+    processError
+};
+
+void connected (Exception * const exc_,
+		void      * const _client)
+{
+    Client * const client = static_cast <Client*> (_client);
+
+    if (exc_) {
+	logE_ (_func, "exc: ", exc_->toString());
+
+	mutex.lock ();
+	destroyClient (client);
+	mutex.unlock ();
+    } else
+	logD_ (_func_);
+}
+
+TcpConnection::Frontend const tcp_conn_frontend = {
+    connected
+};
+
+Result runClient ()
+{
+    Result ret_res = Result::Success;
+
+    dummy_buf = new Byte [dummy_buf_len];
+
+    if (!options.got_server_addr) {
+	if (!setIpAddress ("127.0.0.1:7777", &options.server_addr)) {
+	    logE_ (_func, "setIpAddress() failed");
+	    ret_res = Result::Failure;
+	    goto _return;
+	}
+    }
+
+    if (!server_app.init ()) {
+	logE_ (_func, "server_app.init() failed: ", exc->toString());
+	ret_res = Result::Failure;
+	goto _return;
+    }
+
+    {
+	Byte id_char = 'a';
+	for (Uint32 i = 0; i < options.num_clients; ++i) {
+	    logD_ (_func, "starting client #", i);
+
+	   Ref<Client> const client = new Client (id_char);
+
+	   client->tcp_conn.setInputFrontend (
+		   Cb<Connection::InputFrontend> (&conn_input_frontend, client, client));
+	   client->tcp_conn.setFrontend (
+		   Cb<TcpConnection::Frontend> (&tcp_conn_frontend, client, client));
+
+	   client->tcp_conn.connect (options.server_addr);
+
+	   client->pollable_key = server_app.getPollGroup()->addPollable (client->tcp_conn.getPollable(),
+									  NULL /* ret_reg */);
+	   if (!client->pollable_key) {
+	       logE_ (_func, "PollGroup::addPollable() failed: ", exc->toString());
+	       continue;
+	   }
+
+	   mutex.lock ();
+	   client_list.append (client);
+	   mutex.unlock ();
+	   client->ref ();
+
+	   if (id_char == 'z')
+	       id_char = 'a';
+	   else
+	       ++id_char;
+	}
+    }
+
+    logI_ (_func, "Starting...");
+    if (!server_app.run ())
+	logE_ (_func, "server_app.run() failed: ", exc->toString());
+    logI_ (_func, "...Finished");
+
+    {
+	ClientList::iter iter (client_list);
+	while (!client_list.iter_done (iter)) {
+	    Client * const client = client_list.iter_next (iter);
+	    destroyClient (client);
+	}
+    }
+
+_return:
+    delete[] dummy_buf;
+    dummy_buf = NULL;
+
+    return ret_res;
+}
+
+bool cmdline_help (char const * /* short_name */,
+		   char const * /* long_naem */,
+		   char const * /* value */,
+		   void       * /* opt_data */,
+		   void       * /* cb_data */)
+{
+    options.help = true;
+    return true;
+}
+
+bool cmdline_server_addr (char const * /* short_name */,
+			  char const * /* long_name */,
+			  char const *value,
+			  void       * /* opt_data */,
+			  void       * /* cb_data */)
+{
+    if (!setIpAddress_default (ConstMemory (value, strlen (value)),
+			       "127.0.0.1" /* default_host */,
+			       7777 /* default_port */,
+			       false /* allow_any_host */,
+			       &options.server_addr))
+    {
+	errs->print ("Invalid value \"", value, "\" "
+		     "for --server-addr (IP:PORT expected)");
+	exit (EXIT_FAILURE);
+    }
+    options.got_server_addr = true;
+    return true;
+}
+
+bool cmdline_num_clients (char const * /* short_name */,
+			  char const * /* long_name */,
+			  char const *value,
+			  void       * /* opt_data */,
+			  void       * /* cb_data */)
+{
+    if (!strToUint32_safe (value, &options.num_clients)) {
+	errs->print ("Invalid value \"", value, "\" "
+		     "for --num-clients (number expected): ", exc->toString());
+	exit (EXIT_FAILURE);
+    }
+    return true;
+}
+
+bool cmdline_report_interval (char const * /* short_name */,
+			      char const * /* long_name */,
+			      char const *value,
+			      void       * /* opt_data */,
+			      void       * /* cb_data */)
+{
+    if (!strToUint32_safe (value, &options.report_interval)) {
+	errs->print ("Invalid value \"", value, "\" "
+		     "for --report-interval (number expected): ", exc->toString());
+	exit (EXIT_FAILURE);
+    }
+    return true;
+}
+
+}
+
+int main (int    argc,
+	  char **argv)
+{
+    MyCpp::myCppInit ();
+    libMaryInit ();
+
+    {
+	unsigned const num_opts = 4;
+	MyCpp::CmdlineOption opts [num_opts];
+
+	opts [0].short_name   = "h";
+	opts [0].long_name    = "help";
+	opts [0].opt_callback = cmdline_help;
+
+	opts [1].short_name   = "s";
+	opts [1].long_name    = "server-addr";
+	opts [1].with_value   = true;
+	opts [1].opt_callback = cmdline_server_addr;
+
+	opts [2].short_name   = "n";
+	opts [2].long_name    = "num-clients";
+	opts [2].with_value   = true;
+	opts [2].opt_callback = cmdline_num_clients;
+
+	opts [3].short_name   = "r";
+	opts [3].long_name    = "report-interval";
+	opts [3].with_value   = true;
+	opts [3].opt_callback = cmdline_report_interval;
+
+	MyCpp::ArrayIterator<MyCpp::CmdlineOption> opts_iter (opts, num_opts);
+	MyCpp::parseCmdline (&argc, &argv, opts_iter, NULL /* callback */, NULL /* callback_data */);
+    }
+
+    if (options.help) {
+	printUsage ();
+	return 0;
+    }
+
+    if (runClient ())
+	return 0;
+
+    return EXIT_FAILURE;
+}
+
---
+++ src/server.cpp
@@ -0,0 +1,418 @@
+#include <libmary/libmary.h>
+
+#include <mycpp/mycpp.h>
+#include <mycpp/cmdline.h>
+
+#include <cstdlib>
+
+
+using namespace M;
+
+namespace {
+
+class Options
+{
+public:
+    bool help;
+
+    Uint32 frame_duration;
+    Uint32 frame_size;
+    Uint32 burst_width;
+
+    bool got_server_addr;
+    IpAddress server_addr;
+
+    Options ()
+	: help (false),
+
+	  frame_duration (40),
+	  frame_size (2500),
+	  burst_width (1),
+
+	  got_server_addr (false)
+    {
+    }
+};
+
+mt_const Options options;
+
+void printUsage ()
+{
+    outs->print ("Usage: bmeter_srv [options]\n"
+	         "Options:\n"
+		 "  -d --duration <number>  Frame duration in milliseconds (default: 40)\n"
+		 "  -s --size <number>      Frame size in bytes (default: 2500)\n"
+		 "  -b --burst <number>     Burst width (number of frames to send with zero interval (default: 1)\n"
+		 "  --bind <address>        ip_address:tcp_port to listen (default: *:7777)\n"
+		 "  -h --help               Show help message.\n");
+    outs->flush ();
+}
+
+class ClientSessionList_name;
+
+class ClientSession : public Object,
+		      public IntrusiveListElement<ClientSessionList_name>
+{
+public:
+    bool valid;
+
+    TcpConnection tcp_conn;
+    DeferredConnectionSender deferred_sender;
+    DeferredProcessor::Registration deferred_reg;
+
+    mt_const PollGroup::PollableKey pollable_key;
+
+    ClientSession ()
+	: valid (true),
+	  tcp_conn (this),
+	  deferred_sender (this)
+    {
+    }
+};
+
+typedef IntrusiveList<ClientSession, ClientSessionList_name> ClientSessionList;
+
+mt_mutex (mutex) ClientSessionList session_list;
+
+Mutex mutex;
+
+mt_mutex (tick_mutex) unsigned page_fill_counter = 0;
+
+Mutex tick_mutex;
+
+ServerApp server_app (NULL);
+PagePool page_pool (4096 /* page_size */, 4096 /* min_pages */);
+TcpServer tcp_server (NULL);
+
+mt_mutex (mutex) void destroySession (ClientSession * const session)
+{
+    if (!session->valid)
+	return;
+    session->valid = false;
+
+    server_app.getPollGroup()->removePollable (session->pollable_key);
+    session->deferred_reg.release ();
+
+    session_list.remove (session);
+    session->unref ();
+}
+
+void processError (Exception * const exc_,
+		   void      * const _session)
+{
+    ClientSession * const session = static_cast <ClientSession*> (_session);
+
+    if (exc_)
+	logD_  (_func, "exc: ", exc_->toString());
+    else
+	logD_ (_func_);
+
+    mutex.lock ();
+    destroySession (session);
+    mutex.unlock ();
+}
+
+Connection::InputFrontend const conn_input_frontend = {
+    NULL /* processInput */,
+    processError
+};
+
+void senderClosed (Exception * const exc_,
+		   void      * const _session)
+{
+    ClientSession * const session = static_cast <ClientSession*> (_session);
+
+    if (exc_)
+	logD_  (_func, "exc: ", exc_->toString());
+    else
+	logD_ (_func_);
+
+    mutex.lock ();
+    destroySession (session);
+    mutex.unlock ();
+}
+
+Sender::Frontend const sender_frontend = {
+    NULL /* sendStateChanged */,
+    senderClosed
+};
+
+void frameTimerTick (void * const /* cb_data */)
+{
+    tick_mutex.lock ();
+
+    for (Uint64 i = 0; i < options.burst_width; ++i) {
+	PagePool::PageListHead page_list;
+	{
+	  // Filling page_list.
+
+	    page_pool.getPages (&page_list, options.frame_size);
+
+	    {
+		PagePool::Page *page = page_list.first;
+		while (page) {
+		    memset (page->getData(), (int) page_fill_counter, page->data_len);
+		    page = page->getNextMsgPage();
+		}
+	    }
+
+	    if (page_fill_counter < 255)
+		++page_fill_counter;
+	    else
+		page_fill_counter = 0;
+	}
+
+	ClientSessionList::iter iter (session_list);
+	while (!session_list.iter_done (iter)) {
+	    ClientSession * const session = session_list.iter_next (iter);
+
+	    Sender::MessageEntry_Pages * const msg_pages =
+		    Sender::MessageEntry_Pages::createNew (0 /* max_header_len */);
+	    msg_pages->header_len = 0;
+	    msg_pages->page_pool = &page_pool;
+	    msg_pages->first_page = page_list.first;
+	    msg_pages->msg_offset = 0;
+
+	    page_pool.msgRef (page_list.first);
+	    session->deferred_sender.sendMessage (msg_pages);
+	    session->deferred_sender.flush ();
+	}
+
+	page_pool.msgUnref (page_list.first);
+    }
+
+    tick_mutex.unlock ();
+}
+
+bool acceptOneConnection ()
+{
+    Ref<ClientSession> const session = grab (new ClientSession);
+
+    {
+	TcpServer::AcceptResult const res = tcp_server.accept (&session->tcp_conn);
+	if (res == TcpServer::AcceptResult::Error) {
+	    logE_ (_func, "tcp_server.accept() failed: ", exc->toString());
+	    return false;
+	}
+
+	if (res == TcpServer::AcceptResult::NotAccepted)
+	    return false;
+
+	assert (res == TcpServer::AcceptResult::Accepted);
+    }
+
+    session->tcp_conn.setInputFrontend (
+	    Cb<Connection::InputFrontend> (&conn_input_frontend, session, session));
+
+    session->deferred_sender.setConnection (&session->tcp_conn);
+    session->deferred_sender.setDeferredRegistration (&session->deferred_reg);
+    session->deferred_sender.setFrontend (
+	    Cb<Sender::Frontend> (&sender_frontend, session, session));
+
+    session->pollable_key = server_app.getPollGroup()->addPollable (session->tcp_conn.getPollable(),
+								    &session->deferred_reg);
+    if (!session->pollable_key) {
+	logE_ (_func, "PollGroup::addPollable() failed: ", exc->toString());
+	return true;
+    }
+
+    mutex.lock ();
+    session_list.append (session);
+    mutex.unlock ();
+    session->ref ();
+
+    return true;
+}
+
+void accepted (void * /* cb_data */)
+{
+    logD_ (_func, "Connection accepted");
+
+    while (acceptOneConnection ());
+}
+
+TcpServer::Frontend const tcp_server_frontend = {
+    accepted
+};
+
+Result runServer ()
+{
+    if (!options.got_server_addr) {
+	if (!setIpAddress (":7777", &options.server_addr)) {
+	    logE_ (_func, "setIpAddress() failed");
+	    return Result::Failure;
+	}
+    }
+
+    if (!server_app.init ()) {
+	logE_ (_func, "server_app.init() failed: ", exc->toString());
+	return Result::Failure;
+    }
+
+    if (!tcp_server.open ()) {
+	logE_ (_func, "tcp_server.open() failed: ", exc->toString());
+	return Result::Failure;
+    }
+
+    tcp_server.setFrontend (Cb<TcpServer::Frontend> (&tcp_server_frontend, NULL, NULL));
+
+    if (!tcp_server.bind (options.server_addr)) {
+	logE_ (_func, "tcp_server.bind() failed: ", exc->toString());
+	return Result::Failure;
+    }
+
+    if (!tcp_server.listen ()) {
+	logE_ (_func, "tcp_server.listen() failed: ", exc->toString());
+	return Result::Failure;
+    }
+
+    server_app.getPollGroup()->addPollable (tcp_server.getPollable(), NULL /* ret_reg */);
+
+    server_app.getTimers()->addTimer_microseconds (frameTimerTick,
+						   NULL /* cb_data */,
+						   NULL /* coderef_container */,
+						   (Time) (options.frame_duration * 1000 * options.burst_width),
+						   true /* periodical */);
+
+    logI_ (_func, "Starting...");
+    if (!server_app.run ())
+	logE_ (_func, "server_app.run() failed: ", exc->toString());
+    logI_ (_func, "...Finished");
+
+    {
+	ClientSessionList::iter iter (session_list);
+	while (!session_list.iter_done (iter)) {
+	    ClientSession * const session = session_list.iter_next (iter);
+	    destroySession (session);
+	}
+    }
+
+    return Result::Success;
+}
+
+bool cmdline_help (char const * /* short_name */,
+		   char const * /* long_name */,
+		   char const * /* value */,
+		   void       * /* opt_data */,
+		   void       * /* cb_data */)
+{
+    options.help = true;
+    return true;
+}
+
+bool cmdline_duration (char const * /* short_name */,
+		       char const * /* long_name */,
+		       char const *value,
+		       void       * /* opt_data */,
+		       void       * /* cb_data */)
+{
+    if (!strToUint32_safe (value, &options.frame_duration)) {
+	errs->print ("Invalid value \"", value, "\" "
+		     "for --duration (number expected): ", exc->toString());
+	exit (EXIT_FAILURE);
+    }
+    return true;
+}
+
+bool cmdline_size (char const * /* short_name */,
+		   char const * /* long_name */,
+		   char const *value,
+		   void       * /* opt_data */,
+		   void       * /* cb_data */)
+{
+    if (!strToUint32_safe (value, &options.frame_size)) {
+	errs->print ("Invalid value \"", value, "\" "
+		     "for --size (number expected): ", exc->toString());
+	exit (EXIT_FAILURE);
+    }
+    return true;
+}
+
+bool cmdline_burst (char const * /* short_name */,
+		    char const * /* long_name */,
+		    char const *value,
+		    void       * /* opt_data */,
+		    void       * /* cb_data */)
+{
+    if (!strToUint32_safe (value, &options.burst_width)) {
+	errs->print ("Invalid value \"", value, "\" "
+		     "for --burst (number expected): ", exc->toString());
+	exit (EXIT_FAILURE);
+    }
+    return true;
+}
+
+bool cmdline_bind (char const * /* short_name */,
+		   char const * /* long_name */,
+		   char const *value,
+		   void       * /* opt_data */,
+		   void       * /* cb_data */)
+{
+    if (!setIpAddress_default (ConstMemory (value, strlen (value)),
+			       ConstMemory(),
+			       7777 /* default_port */,
+			       true /* allow_any_host */,
+			       &options.server_addr))
+    {
+	errs->print ("Invalid value \"", value, "\" "
+		     "for --bind (IP:PORT expected)");
+	exit (EXIT_FAILURE);
+    }
+    options.got_server_addr = true;
+    return true;
+}
+
+}
+
+int main (int    argc,
+	  char **argv)
+{
+    MyCpp::myCppInit ();
+    libMaryInit ();
+
+    {
+	unsigned const num_opts = 5;
+	MyCpp::CmdlineOption opts [num_opts];
+
+	opts [0].short_name   = "h";
+	opts [0].long_name    = "help";
+	opts [0].opt_callback = cmdline_help;
+
+	opts [1].short_name = "d";
+	opts [1].long_name = "duration";
+	opts [1].with_value = true;
+	opts [1].opt_callback = cmdline_duration;
+
+	opts [2].short_name = "s";
+	opts [2].long_name = "size";
+	opts [2].with_value = true;
+	opts [2].opt_callback = cmdline_size;
+
+	opts [3].short_name = "b";
+	opts [3].long_name = "burst";
+	opts [3].with_value = true;
+	opts [3].opt_callback = cmdline_burst;
+
+	opts [4].long_name = "bind";
+	opts [4].with_value = true;
+	opts [4].opt_callback = cmdline_bind;
+
+	MyCpp::ArrayIterator<MyCpp::CmdlineOption> opts_iter (opts, num_opts);
+	MyCpp::parseCmdline (&argc, &argv, opts_iter, NULL /* callback */, NULL /* callback_data */);
+    }
+
+    if (options.help) {
+	printUsage ();
+	return 0;
+    }
+
+    errs->print ("Frame duration: ", options.frame_duration, "\n"
+		 "Frame size:     ", options.frame_size, "\n"
+		 "Burst width:    ", options.burst_width, "\n");
+
+    if (runServer ())
+	return 0;
+
+    return EXIT_FAILURE;
+}
+
